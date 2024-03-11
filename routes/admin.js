a1ed4c777bddd4cf18ca02a341ec32ddcb22a198
Sealed <korn2400@gmail.com>
2024-03-11 11:46:33 +0700

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 router.get("/add-product", (req, res, next) => {
-	console.log("Say Hi from store");
-	res.send(
-		'<body><form action ="/admin/add-product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form></body>'
-	);
+	// res.sendFile(path.join(__dirname, "../views/add-product.html"));
+	// res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+	res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.use("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 router.get("/", (req, res, next) => {
-	res.send("<h1>Welcome to my shop</h1>");
+	// res.sendFile(path.join(__dirname, "../views/shop.html"));
+	res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
